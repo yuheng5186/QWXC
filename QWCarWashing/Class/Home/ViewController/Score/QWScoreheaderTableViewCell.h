9cd3346a33bccadaafcbbd01566fcc5e3654ef0e
”ymz520aa“ <2695570680@qq.com>
2017-08-21 23:46:40 +0800

    蔷薇会员界面修改

--- QWCarWashing/Class/Home/ViewController/Score/QWScoreheaderTableViewCell.h
+++ QWCarWashing/Class/Home/ViewController/Score/QWScoreheaderTableViewCell.h
@@ -9,20 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface QWScoreheaderTableViewCell : UITableViewCell
-@property (weak, nonatomic) IBOutlet UIImageView *headerImage;
-
+@property (weak, nonatomic)IBOutlet  UIImageView *headerImage;
+@property (weak, nonatomic) IBOutlet UIView *contentViews;
+@property (weak, nonatomic) IBOutlet UILabel *Maxline;
+@property (weak, nonatomic) IBOutlet UILabel *linev;
+//
 @property (weak, nonatomic) IBOutlet UILabel *phoneNum;
 @property (weak, nonatomic) IBOutlet UIButton *vipType;
 @property (weak, nonatomic) IBOutlet UIButton *goUpGrade;
 @property (weak, nonatomic) IBOutlet UIButton *ScoreNum;
 @property (weak, nonatomic) IBOutlet UIButton *AddScore;
-@property(nonatomic,copy) void (^viptypeonclick)(UIButton *);
-@property(nonatomic,copy) void (^goupgradeonclick)(UIButton *);
-@property(nonatomic,copy) void (^scorenum)(UIButton *);
-@property(nonatomic,copy) void (^addscore)(UIButton *);
 
-@property (weak, nonatomic) IBOutlet UIImageView *bgImage;
-- (IBAction)scoreNumOnclick:(id)sender;
+
+@property (nonatomic, strong) CAGradientLayer *gradientLayer;
+//- (IBAction)scoreNumOnclick:(id)sender;
 
 @end
 
