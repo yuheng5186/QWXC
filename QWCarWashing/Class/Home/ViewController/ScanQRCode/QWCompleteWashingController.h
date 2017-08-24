d8b413d20abb58f2f203cebb0a89116ec2fcdd32
Mac WuXinLing <218100345@qq.com>
2017-08-24 15:55:47 +0800

    扫码洗车流程

---
+++ QWCarWashing/Class/Home/ViewController/ScanQRCode/QWCompleteWashingController.h
@@ -0,0 +1,13 @@
+//
+//  QWCompleteWashingController.h
+//  QWCarWashing
+//
+//  Created by Mac WuXinLing on 2017/8/24.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "QWBaseViewController.h"
+
+@interface QWCompleteWashingController : QWBaseViewController
+
+@end
