9f7a68d44ddb2ffcc968b2547fb464d0626c151c
Mac WuXinLing <218100345@qq.com>
2017-08-25 14:33:36 +0800

    蔷薇分享微信

--- QWCarWashing/AppDelegate.h
+++ QWCarWashing/AppDelegate.h
@@ -8,7 +8,10 @@
 
 #import <UIKit/UIKit.h>
 #import "QWUserInfo.h"
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#import "WXApi.h"
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,WXApiDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) QWUserInfo *currentUser;
