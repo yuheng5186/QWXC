b28908b4b0767212b351007e7f2e0a55404ed15c
Mac WuXinLing <218100345@qq.com>
2017-09-25 16:16:11 +0800

    新增接口

--- QWCarWashing/Class/Tool/AFNetworkingtool/HTTPDefine.h
+++ QWCarWashing/Class/Tool/AFNetworkingtool/HTTPDefine.h
@@ -7,6 +7,10 @@
 //#define kHTTPImg @"http://192.168.3.80:8090"
 //
 #pragma mark-服务器
+//#define Khttp @"http://192.168.2.152:8090/api/"
+//
+//#define kHTTPImg @"http://192.168.2.152:8090"
+
 #define Khttp @"http://119.23.53.225:8090/api/"
 
 #define kHTTPImg @"http://119.23.53.225:8090"
